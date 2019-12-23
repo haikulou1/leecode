91a674a389553010d29249fc2738798bcd93b0ef
haikulou1 <920937242@qq.com>
2019-12-23 22:13:14 +0800

    2019-12-23

--- leecode/src/Leecode2/Solution.java
+++
@@ -1,7 +0,0 @@
-package Leecode2;
-
-/**
- * Created by 胡歌的小迷弟 on 2019/12/23 21:42
- */
-public class Solution {
-}
--- leecode/src/two_sum_001/Solution.java
+++ leecode/src/two_sum_001/Solution.java
@@ -1,4 +1,4 @@
-package Leecode1;
+package two_sum_001;
 
 /**
  * Created by 胡歌的小迷弟 on 2019/12/23 21:41
